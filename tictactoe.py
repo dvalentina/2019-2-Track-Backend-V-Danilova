fc028b11a507bc5a8c06eef3985e732ddbfe3bb3
dvalentina <theviolintea@gmail.com>
2019-10-10 19:24:05 +0300

    HW 2 - titactoe game and tests

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python3
 
-class Tictactoe(object):
+'''Tic-tac-toe game'''
+
+class Tictactoe():
+    '''Tic-tac-toc game class'''
+
     def __init__(self):
         self.board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         self.x_move = True
@@ -8,10 +12,12 @@ class Tictactoe(object):
         self.player_o_name = 'Player O'
 
     def start(self):
+        '''Starting game'''
         self.hello()
         self.play()
-    
+
     def hello(self):
+        '''Indroduction'''
         print("Hello! I'm the Tic-tac-toe game.")
         print("Player X, do you want to introduce yourself? (y/n)")
         answer = input()
@@ -32,32 +38,36 @@ class Tictactoe(object):
         print("We are ready to start!")
         print("To make a move, enter a number of the preferred cell.")
         print(f"{self.player_x_name} makes the first move.")
-    
+
     def play(self):
+        '''Game process'''
         for i in range(9):
             self.draw()
-            self.makeMove()
-            if i >= 4 and self.checkWin() == True:
+            self.make_move()
+            if i >= 4 and self.check_win() is True:
                 self.end(True)
                 break
-            elif i == 8 and self.checkWin() == False:
+            if i == 8 and self.check_win() is False:
                 self.end(False)
             else:
-                self.changePlayer()
-            
+                self.change_player()
+
     def draw(self):
+        '''Draws a board in terminal'''
         print("=====")
         for i in range(3):
             print(f"{self.board[i * 3]}|{self.board[i * 3 + 1]}|{self.board[i * 3 + 2]}")
-        print("=====")       
-        
-    def changePlayer(self):
+        print("=====")
+
+    def change_player(self):
+        '''Changes the order of move'''
         if self.x_move:
             self.x_move = False
         else:
             self.x_move = True
 
-    def makeMove(self):
+    def make_move(self):
+        '''Receives a move of a player'''
         if self.x_move:
             print(f"{self.player_x_name}, make your move.")
             symbol = 'X'
@@ -66,24 +76,25 @@ class Tictactoe(object):
             symbol = 'O'
         while True:
             number_of_cell = input()
-            if self.checkCorrectInput(number_of_cell):
+            if self.check_correct_input(number_of_cell):
                 self.board[int(number_of_cell) - 1] = symbol
                 break
-            else:
-                print("Input is incorrect, please try again.")
+            print("Input is incorrect, please try again.")
 
-    def checkCorrectInput(self, input):
+    def check_correct_input(self, some_input):
+        '''Checks, if the input is correct'''
         is_correct = False
         for i in range(9):
-            if (input == f'{self.board[i]}' and 
+            if (some_input == f'{self.board[i]}' and
                     self.board[i] != 'X' and
                     self.board[i] != 'O'):
                 is_correct = True
         return is_correct
-               
-    def checkWin(self):
+
+    def check_win(self):
+        '''Checks, if one of the players wins the game'''
         for i in range(3):
-            if (self.board[i * 3] == self.board[i * 3 + 1] == self.board[i * 3 + 2] or 
+            if (self.board[i * 3] == self.board[i * 3 + 1] == self.board[i * 3 + 2] or
                     self.board[i] == self.board[i + 3] == self.board[i + 6] or
                     self.board[0] == self.board[4] == self.board[8] or
                     self.board[2] == self.board[4] == self.board[6]):
@@ -92,6 +103,7 @@ class Tictactoe(object):
 
 
     def end(self, if_someone_wins):
+        '''Conclusion of the game'''
         self.draw()
         if if_someone_wins:
             if self.x_move:
@@ -101,7 +113,7 @@ class Tictactoe(object):
         else:
             print("That's a draw!")
         print("Thanks for a good game.")
-            
+
 if __name__ == "__main__":
     game = Tictactoe()
     game.start()
--- tictactoe_tests.py
+++ tictactoe_tests.py
@@ -1,98 +1,99 @@
 #! /usr/bin/env python3
 
+'''Tests for Tic-tac-toe game'''
+
+from unittest import mock
 from tictactoe import Tictactoe
 
-# ===============================
-# tests
-# ===============================
-
-def initTest():
+def init_test():
+    '''Tests initialization'''
     game = Tictactoe()
     is_passed = True
     for i in range(9):
         if (game.board[i] != i + 1 or
-                game.x_move != True or
+                game.x_move is not True or
                 game.player_x_name != 'Player X' or
                 game.player_o_name != 'Player O'):
             is_passed = False
-    assert is_passed == True
-    print("initTest passed")
+    assert is_passed is True
+    print("init_test passed")
 
-def changePlayerTest():
+def change_player_test():
+    '''Tests changing order of moves'''
     game = Tictactoe()
-    game.changePlayer()
-    assert game.x_move == False
-    game.changePlayer()
-    assert game.x_move == True
-    print("changePlayerTest passed")
+    game.change_player()
+    assert game.x_move is False
+    game.change_player()
+    assert game.x_move is True
+    print("change_player_test passed")
 
-def checkWinTest():
+def check_win_test():
+    '''Tests checking if someone wins'''
     game = Tictactoe()
     game.board = ['X', 2, 'O', 'X', 5, 'O', 'X', 8, 9]
     # X 2 O
     # X 5 O
     # X 8 9
-    assert game.checkWin() == True
+    assert game.check_win() is True
     game.board = ['X', 2, 'O', 'X', 5, 'O', 7, 'X', 9]
     # X 2 O
     # X 5 O
     # 7 X 9
-    assert game.checkWin() == False
+    assert game.check_win() is False
     game.board = ['O', 'O', 'O', 'X', 5, 'X', 7, 'X', 9]
     # O O O
     # X 5 X
     # 7 X 9
-    assert game.checkWin() == True 
+    assert game.check_win() is True
     game.board = ['O', 2, 3, 'X', 'O', 6, 'X', 'X', 'O']
     # O 2 3
     # X O 6
     # X X O
-    assert game.checkWin() == True  
+    assert game.check_win() is True
     game.board = ['O', 'O', 'X', 'X', 'X', 'O', 'X', 'X', 'O']
     # O O X
     # X X O
     # X X O
-    assert game.checkWin() == True  
+    assert game.check_win() is True
     game.board = ['O', 'X', 'X', 'X', 'O', 'O', 'O', 'X', 'X']
     # O X X
     # X O O
     # O X X
-    assert game.checkWin() == False
-    print("checkWinTest passed")
+    assert game.check_win() is False
+    print("check_win_test passed")
 
-def checkCorrectInputTest():
+def check_correct_input_test():
+    '''Tests checking if the input is correct'''
     game = Tictactoe()
     game.board = ['O', 2, 3, 'X', 'O', 6, 'X', 'X', 'O']
     # O 2 3
     # X O 6
     # X X O
     not_int = 'N'
-    assert game.checkCorrectInput(not_int) == False
+    assert game.check_correct_input(not_int) is False
     too_large_int = '10'
-    assert game.checkCorrectInput(too_large_int) == False
+    assert game.check_correct_input(too_large_int) is False
     too_small_int = '0'
-    assert game.checkCorrectInput(too_small_int) == False
+    assert game.check_correct_input(too_small_int) is False
     already_occupied_cell = '4'
-    assert game.checkCorrectInput(already_occupied_cell) == False
+    assert game.check_correct_input(already_occupied_cell) is False
     already_occupied_cell = '9'
-    assert game.checkCorrectInput(already_occupied_cell) == False
+    assert game.check_correct_input(already_occupied_cell) is False
     correct_input = '3'
-    assert game.checkCorrectInput(correct_input) == True
-    print("checkCorrectInput passed")
+    assert game.check_correct_input(correct_input) is True
+    print("check_correct_input passed")
 
-from unittest import mock
-from unittest.mock import patch
-
-def makeMoveTest():
+def make_move_test():
+    '''Tests if make_move method acts correctly'''
     game = Tictactoe()
-    with mock.patch('builtins.input', return_value = "4"):
-        game.makeMove()
+    with mock.patch('builtins.input', return_value="4"):
+        game.make_move()
         assert game.board[3] == 'X'
-        print("makeMoveTest passed")
+        print("make_move_test passed")
 
 if __name__ == "__main__":
-    initTest()
-    changePlayerTest()
-    makeMoveTest()
-    checkCorrectInputTest()
-    checkWinTest()
+    init_test()
+    change_player_test()
+    make_move_test()
+    check_correct_input_test()
+    check_win_test()
